7f25d8a4bf67bf7282ab3b2a2803760f56df1104
sai santhosh <saiprakash75746@gmail.com>
2026-03-26 10:01:29 +0530

    changes 4

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -12,7 +11,6 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -35,6 +33,10 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/friends", friendRoutes);
 app.use("/api/*", (req, res) => {
   res.status(404).json({ message: "API route not found" });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Chatsuu backend is live" });
 });
 
 if (process.env.NODE_ENV === "production") {
